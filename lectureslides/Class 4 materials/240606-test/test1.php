2a9b68026ee3925c4407b169f5a482ae3a67838c
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-06-06 11:18:20 +0900

    Delete lectureslides/Class 4 materials directory

--- lectureslides/Class 4 materials/240606-test/test1.php
+++
@@ -1,6 +0,0 @@
-<?php
-$foo = 25;
-$bar = "Hello";
-$foo = ($foo * 7);
-echo "Here";
-?>
--- lectureslides/Class 4 materials/240606-test/test20240606.php
+++
@@ -1,25 +0,0 @@
-<?php
-//echo phpinfo();
-//exit;
-
-echo "Hi, I'm a PHP script!"; ?>
-<?php
-echo "<br><br><br>Here are the comment in php</p>";
-//php comment
-#php comment
-/*php block comment
-php block comment
-php block comment*/
-
-echo "<h1>This is H1</h1>";
-?>
-
-
-<?php
-$foo = 25;		// Numerical variable
-$bar = "Hello";	// String variable
-$foo = ($foo * 7);	// Multiplies foo by 7
-//$bar = ($bar * 7);	// Invalid expression
-echo "Here";
-echo $foo;
-?>
