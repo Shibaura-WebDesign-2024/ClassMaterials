1d6500cf9e02940dffb58cdf260bc79dc7a7d428
Manishk <167336534+Manssz@users.noreply.github.com>
2024-06-19 14:22:55 +0900

    Add files via upload

---
+++ lectureslides/Assignment-6-main/Assignment-6-main/material/part2/view/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>My Guitar Shop</title>
+    <link rel="stylesheet" type="text/css"
+          href="../main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>My Guitar Shop</h1></header>
+
+
