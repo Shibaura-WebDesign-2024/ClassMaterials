efe4c9a066e0a2cc8064a0e550099cd7ec79cd19
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-06-20 12:43:55 +0900

    Delete lectureslides/Assignment-6-main directory

--- lectureslides/Assignment-6-main/material/part1/errors/error.php
+++
@@ -1,6 +0,0 @@
-<?php include '../view/header.php'; ?>
-<div id="main">
-    <h1 class="top">Error</h1>
-    <p class="first_paragraph"><?php echo $error; ?></p>
-</div>
-<?php include '../view/footer.php'; ?>
